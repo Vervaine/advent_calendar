028c8bbf16d02d3d6d7e5d12c33abecb3afeb459
jeremie.rossier2 <jeremie.rossier2@edu.hefr.ch>
2022-11-10 11:36:05 +0100

    Fix assets path

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,7 @@ git init
 git checkout -B master
 git add -A
 git commit -m 'deploy'
+git push -f git@github.com:vervaine/advent_calendar.git master:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
