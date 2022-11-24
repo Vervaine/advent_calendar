f441b68d079eb35f16f07610142c5ca9623482a9
jeremie.rossier2 <jeremie.rossier2@edu.hefr.ch>
2022-11-24 20:44:46 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,17 @@
 set -e
 
 # build
-npm run build
 
 # navigate into the build output directory
-cd dist
 
 # place .nojekyll to bypass Jekyll processing
 echo > .nojekyll
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+cd dist
+
+npm run build
 
 git init
 git checkout -B master
